d67b73600362c8f5eef21ebf9ff4f609c68242c4
ROHIT SIKARWAR <rohitsikarwar1998@gmail.com>
2020-11-14 23:36:27 +0530

    initial setup

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/macro';
+
+export const Inner = styled.div`
+  display: flex;
+  font-size: 50px;
+  justify-content: center;
+`;
+
+export const Pane = styled.div``;
