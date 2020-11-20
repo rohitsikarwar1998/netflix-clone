56dec2afa3fb2939130fa312ac5d628a65042eca
ROHIT SIKARWAR <rohitsikarwar1998@gmail.com>
2020-11-20 12:57:09 +0530

    jumbotron completed

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,10 +1,54 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Inner } from './styles/jumbotron';
+import PropTypes from 'prop-types';
+import {
+  Item,
+  Pane,
+  Container,
+  Inner,
+  Title,
+  SubTitle,
+  Image,
+} from './styles/jumbotron';
 
-export default function Jumbotron() {
+export default function Jumbotron({
+  children,
+  direction = 'row',
+  ...restProps
+}) {
   return (
-    <Inner>
-      <p>Welcome!</p>
-    </Inner>
+    <Item>
+      <Inner direction={direction} {...restProps}>
+        {children}
+      </Inner>
+    </Item>
   );
 }
+
+Jumbotron.Container = ({ children, ...restProps }) => (
+  <Container {...restProps}>{children}</Container>
+);
+Jumbotron.Pane = ({ children, ...restProps }) => (
+  <Pane {...restProps}>{children}</Pane>
+);
+Jumbotron.Title = ({ children, ...restProps }) => (
+  <Title {...restProps}>{children}</Title>
+);
+Jumbotron.SubTitle = ({ children, ...restProps }) => (
+  <SubTitle {...restProps}>{children}</SubTitle>
+);
+Jumbotron.Image = ({ ...restProps }) => <Image {...restProps} />;
+
+// jumbotron prop types object
+const propTypes = {
+  children: PropTypes.element.isRequired,
+  direction: PropTypes.string.isRequired,
+};
+
+// apply above proptypes to every component
+Jumbotron.propTypes = propTypes;
+Jumbotron.Container.propTypes = propTypes;
+Jumbotron.Pane.propTypes = propTypes;
+Jumbotron.Title.propTypes = propTypes;
+Jumbotron.SubTitle.propTypes = propTypes;
+Jumbotron.Image.propTypes = propTypes;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { GlobalStyles } from './global-styles';
+import 'normalize.css';
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <GlobalStyles />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </>,
   document.getElementById('root'),
 );
